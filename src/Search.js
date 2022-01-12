4bfede4d372607df15ca3f684e3c0094c8196c87
Edgar Manuel Peralta Sánchez <edperalta.c02@ucamp.utel.edu.mx>
2022-01-11 22:22:58 -0700

    Agregando PanelAdd.js

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
-function Search(props){
+class Search extends React.Component{
 
-  return(
-   <input type="text" />
-  );
+    constructor(props){
+        super(props);
+
+        this.onChangeEvent = this.onChangeEvent.bind(this);
+    }
+
+    onChangeEvent(e){
+        const query = e.target.value.toString().toLowerCase();
+        this.props.onsearch(e.target.value);
+    }
+
+    render(){
+        return(
+            <input type="text" onChange={this.onChangeEvent} />
+        );
+    }
 }
 
 export default Search;
