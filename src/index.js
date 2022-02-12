599ba980009fc66159ce80f956335bd97ba5b4df
Edgar Manuel Peralta Sánchez <edperalta.c02@ucamp.utel.edu.mx>
2022-02-12 13:56:04 -0700

    Cambios

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/Proyecto-5-Ecommerce-App-Libros'>
       <UserProvider>
         <App />
       </UserProvider>
